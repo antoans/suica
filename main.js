1c8e06260df14ed87d780b63da7124c81496a727
antoans <totokn@abv.bg>
2018-02-08 00:26:55 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $( window ).on( "load", function() {
 	
 	orthographic(1,2);
 	lookAt([0,0,1], [0,0,0], [0,1,0]);
-	suica1.gl.canvas.addEventListener('mousedown', onMouseDown);
+	suica1.gl.canvas.addEventListener('mousedown', onMouseDownSuica1);
 	suica1.gl.canvas.addEventListener('mouseup', onMouseUp);
 	suica1.gl.canvas.addEventListener('mousemove', onMouseMove);
 	
@@ -11,8 +11,8 @@ $( window ).on( "load", function() {
 	var b = point([60,150,0]).custom({color: [0,0.6,0], interactive: true, pointSize: 15});
 	var c = point([-350,150,0]).custom({color: [0,0,0]});
 	
-	moveLetters();
-	calcAngles();
+	moveLettersOnTriangle();
+	calcAnglesOnTriangle();
 	
 	segment(a.center, b.center).custom({color: [0.4,0.4,0.4]});
 	segment(a.center, c.center).custom({color: [0.4,0.4,0.4]});
@@ -32,22 +32,34 @@ $( window ).on( "load", function() {
 	
 	orthographic(1,2);
 	lookAt([0,0,1], [0,0,0], [0,1,0])
-	suica2.gl.canvas.addEventListener('mousedown', onMouseDown);
+	suica2.gl.canvas.addEventListener('mousedown', onMouseDownSuica2);
 	suica2.gl.canvas.addEventListener('mouseup', onMouseUp);
 	suica2.gl.canvas.addEventListener('mousemove', onMouseMove);
 	
+	line([0,0,0],[0,1,0]).custom({color: [0,0,0]});
+	line([0,0,0],[1,0,0]).custom({color: [0,0,0]});
 	
+	circ = circle([0,0,0],250).custom({mode:Suica.LINE, color: [0,0.6,0]});
 	
+	var p = point([0,250,0]).custom({pointSize: 15, interactive: true, color: [0,0.6,0]});
+	segment(p.center, circ.center).custom({color: [1,0,0]});
+	var sin_point = point([0, p.center[1], 0]);
+	var cos_point = point([p.center[0], 0, 0]);
+	segment(sin_point.center, p.center).custom({color: [0.6,0,0.6]});
+	segment(cos_point.center, p.center).custom({color: [0,0,1]});
 	
-	
-	function onFrame() {
-		console.log(c);
-	}
+	moveElementByIdToPoint(suica2, "O_circle", circ.center);
+	moveElementByIdToPoint(suica2, "A_circle", [circ.radius, 0, 0]);
+	moveElementByIdToPoint(suica2, "P_circle", p.center, 0, -30);
 	
 	var clickedPoint = null;
 	
-	function onMouseDown(e) {
+	function onMouseDownSuica1(e) {
 		clickedPoint = suica1.objectAtPoint(e.clientX, e.clientY);
+	}
+	
+	function onMouseDownSuica2(e) {
+		clickedPoint = suica2.objectAtPoint(e.clientX, e.clientY);
 	}
 	
 	function onMouseUp(e) {
@@ -57,76 +69,93 @@ $( window ).on( "load", function() {
 	function onMouseMove(e) {			
 		if (clickedPoint) {
 			movePoint(e);
-			moveLetters();
-			calcAngles();
 		}
 	}
 	
 	function movePoint(e) {
 		var vector;
 		var offset;
-		
-		if (clickedPoint == a) {
-			vector = vec_ac;
-			offset = offset_ac;
-		} else if (clickedPoint == b) {
-			vector = vec_bc;
-			offset = offset_bc;
-		} else {
-			return;
-		}
-		
-		var x = e.clientX - e.target.offsetLeft - e.target.offsetWidth / 2;
-		var y = - ( e.clientY - e.target.offsetTop - e.target.offsetHeight / 2);
-		
-		var d =( (x - c.center[0]) * vector[0] + (y - c.center[1]) * vector[1] ) / offset;
+		var d;
 
-		clickedPoint.center[0] = c.center[0] + d * vector[0];
-		clickedPoint.center[1] = c.center[1] + d * vector[1];
+		var x = e.clientX - e.target.offsetLeft - e.target.offsetWidth / 2;
+		var y = - ( e.clientY - e.target.offsetTop - e.target.offsetHeight / 2) - window.scrollY;
+		
+		if (clickedPoint == a || clickedPoint == b) {
+			vector = clickedPoint == a ? vec_ac : vec_bc;
+			offset = clickedPoint == a ? offset_ac : offset_bc;
+			d =( (x - c.center[0]) * vector[0] + (y - c.center[1]) * vector[1] ) / offset;
+			
+			clickedPoint.center[0] = c.center[0] + d * vector[0];
+			clickedPoint.center[1] = c.center[1] + d * vector[1];
+			
+			moveLettersOnTriangle();
+			calcAnglesOnTriangle();
+			
+			
+		} else if (clickedPoint == p) {
+			d = Math.sqrt( x * x + y * y ); 
+			clickedPoint.center[0] = circ.radius * x / d;
+			clickedPoint.center[1] = circ.radius * y / d;
+			
+			sin_point.center[1] = clickedPoint.center[1];
+			cos_point.center[0] = clickedPoint.center[0];
+			
+			onCirclePointMoved();
+		}
 	}
 	
-	function moveLetters() {
-		var div = document.getElementById("A");
-		var pos = suica1.getPosition(a.center);
-		div.style.left = pos[0] + "px";
-		div.style.top = (pos[1] + 5) + "px";
-		
-		div = document.getElementById("degA");
-		div.style.left = (pos[0] - 0) + "px";
-		div.style.top = (pos[1] - 30 ) + "px";
-		
-		div = document.getElementById("B");
-		pos = suica1.getPosition(b.center);
-		div.style.left = (pos[0] + 10) + "px";
-		div.style.top = pos[1] + "px";
-		
-		
-		div = document.getElementById("degB");
-		div.style.left = (pos[0] - 40) + "px";
-		div.style.top = (pos[1] - 25) + "px";
+	function onCirclePointMoved() {
+		moveElementByIdToPoint(suica2, "P_circle", p.center, 0, -30);
+		moveElementByIdToPoint(suica2, "sin_circle", sin_point.center);
+		moveElementByIdToPoint(suica2, "cos_circle", cos_point.center);
+		document.getElementById("sin_circle").innerHTML = "sin(α)<br>" + ((sin_point.center[1] / circ.radius).toFixed(3));
+		document.getElementById("cos_circle").innerHTML = "cos(α)<br>" + ((cos_point.center[0] / circ.radius).toFixed(3));
 	}
 	
-	function calcAngles() {
+	/** id - string; point - Suica.point; x,y - offset (in pixels) */
+	function moveElementByIdToPoint(suica, id, point, x, y) {
+		if (!x) x = 0;
+		if (!y) y = 0;
+		
+		var div = document.getElementById(id);
+		var pos = suica.getPosition(point);
+		div.style.left = (pos[0] + x) + "px";
+		div.style.top = (pos[1] + y) + "px";
+	}
+	
+	function moveLettersOnTriangle() {
+		moveElementByIdToPoint(suica1, "A", a.center, 0, 5);
+		moveElementByIdToPoint(suica1, "degA", a.center, 0, -30);
+		moveElementByIdToPoint(suica1, "B", b.center, 10, 0);
+		moveElementByIdToPoint(suica1, "degB", b.center, -40, -25);
+		moveElementByIdToPoint(suica1, "C", c.center, -15, -20);
+		moveElementByIdToPoint(suica1, "degC", c.center, 5, 0);
+	}
+	
+	function calcAnglesOnTriangle() {
 		var AB_length = Math.sqrt((a.center[0] - b.center[0]) * (a.center[0] - b.center[0]) + (a.center[1] - b.center[1]) * (a.center[1] - b.center[1]));
 		var AC_length = Math.sqrt((a.center[0] - c.center[0]) * (a.center[0] - c.center[0]) + (a.center[1] - c.center[1]) * (a.center[1] - c.center[1]));
 		var BC_length = Math.sqrt((c.center[0] - b.center[0]) * (c.center[0] - b.center[0]) + (c.center[1] - b.center[1]) * (c.center[1] - b.center[1]));
 
-		var sinB = (AC_length / AB_length);
-		var cosB = (BC_length / AB_length);
-		var sinA = (BC_length / AB_length);
-		var cosA = (AC_length / AB_length);
+		var sinB = AC_length / AB_length;
+		var cosB = BC_length / AB_length;
+		var tgB = sinB / cosB;
+		var cotgB = 1 / tgB;
+		var sinA = BC_length / AB_length;
+		var cosA = AC_length / AB_length;
+		var tgA = sinA / cosA;
+		var cotgA = 1 / tgA;
 		
 		document.getElementById("sinB").innerHTML = sinB.toFixed(3);
 		document.getElementById("cosB").innerHTML = cosB.toFixed(3);
+		document.getElementById("tgB").innerHTML = tgB.toFixed(3);
+		document.getElementById("cotgB").innerHTML = cotgB.toFixed(3);
 		document.getElementById("sinA").innerHTML = sinA.toFixed(3);
 		document.getElementById("cosA").innerHTML = cosA.toFixed(3);
+		document.getElementById("tgA").innerHTML = tgA.toFixed(3);
+		document.getElementById("cotgA").innerHTML = cotgA.toFixed(3);
 		
 		document.getElementById("degA").innerHTML = (Math.asin(sinA)*180/Math.PI).toFixed(1) + "°";
 		document.getElementById("degB").innerHTML = (Math.asin(sinB)*180/Math.PI).toFixed(1) + "°";
 	}
 });
-
-function log(o) {
-	console.log(o);
-}
-
